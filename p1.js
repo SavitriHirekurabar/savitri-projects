7973a6e6b0a20f024077523d6b6cb4a9ad0667a1
SavitriHirekurabar <savitrihirekurabar126@gmail.com>
2026-02-01 06:19:14 +0530

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,23 @@
+// console.log("hello world!!");
+// let a=2;
+// let b=3;
+// res=b*a;
+// console.log(res);
+
+// function myFunc() {
+//     console.log("Hi i am savitri");
+//     console.log("I am from cse dept")
+// }
+// myFunc();
+// myFunc();
+// myFunc();
+// myFunc();
+
+// function sum(a,b){
+//     sum=a*b;
+//     sum=a-b;
+//     sum=a+b;
+//     return sum;
+// }
+// let res=sum(3,4);
+// console.log(res);
